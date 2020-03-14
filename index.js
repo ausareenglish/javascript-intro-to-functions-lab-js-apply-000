63fb38d7af45dbc447dd88619ece01c731c0113e
Tehuti English <ausareenglish@gmail.com>
2020-03-14 09:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
-function logWhisper(string) {
-  
+function logShout(string) {
+  console.log(string.toUpperCase());
 }
